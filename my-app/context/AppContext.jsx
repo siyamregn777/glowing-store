1e959235f279782e284726d0642b475cc5fc2cbd
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-04-11 22:22:48 +0300

    feat: integrate user context from Clerk for authentication

--- my-app/context/AppContext.jsx
+++ my-app/context/AppContext.jsx
@@ -1,7 +1,8 @@
 'use client'
 import { productsDummyData, userDummyData } from "@/assets/assets";
+import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, use, useContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
@@ -14,7 +15,7 @@ export const AppContextProvider = (props) => {
 
     const currency = process.env.NEXT_PUBLIC_CURRENCY
     const router = useRouter()
-
+    const {user} = useUser();
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
     const [isSeller, setIsSeller] = useState(true)
@@ -83,6 +84,7 @@ export const AppContextProvider = (props) => {
     }, [])
 
     const value = {
+        user,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
