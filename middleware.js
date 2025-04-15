1a8a4df6bef9dfee40555b5b71d0fe8b18c6f998
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-04-15 23:54:34 +0300

    refactor: update middleware to use createRouteMatcher and streamline route protection logic

--- middleware.js
+++ middleware.js
@@ -1,12 +1,37 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// };
+
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher([
+  '/seller(.*)',
+  '/add-address',
+  '/cart',
+  '/my-orders'
+]);
+
+export default clerkMiddleware((auth, req) => {
+  // Explicitly allow Inngest endpoint
+  if (req.nextUrl.pathname.startsWith('/api/inngest')) {
+    return; // Skip auth for Inngest
+  }
+  
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
