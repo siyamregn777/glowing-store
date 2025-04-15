3faeca6340e9da3d7e328a64866033b2b9df7a26
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-04-15 22:05:10 +0300

    feat: implement MongoDB connection logic and middleware setup

--- my-app/config/db.js
+++ my-app/config/db.js
@@ -1,26 +1,54 @@
+// import mongoose from "mongoose";
+
+// let catched = global.mongoose;
+
+// if(!catched){
+//     catched = global.mongoose= {conn:null ,promise:null}
+// }
+
+// async function connectDb() {
+//     if(catched.conn){
+//         return catched.conn
+//     }
+//     if(!catched.promise){
+//         const opts = {
+//             bufferCommands:false,
+//         }
+//         catched.promise = mongoose.connect(`${ process.env.MONGODB_URI}/qickcart`,opts).then((mongoose) => {
+//             return mongoose
+//         })
+//     }
+//     catched.conn = await catched.promise;
+//     return catched.conn;
+    
+// }
+
+// export default connectDb;
+
+
 import mongoose from "mongoose";
 
-let catched = global.mongoose;
-
-if(!catched){
-    catched = global.mongoose= {conn:null ,promise:null}
-}
+const connection = {};
 
 async function connectDb() {
-    if(catched.conn){
-        return catched.conn
-    }
-    if(!catched.promise){
-        const opts = {
-            bufferCommands:false,
-        }
-        catched.promise = mongoose.connect(`${ process.env.MONGODB_URI}/qickcart`,opts).then((mongoose) => {
-            return mongoose
-        })
-    }
-    catched.conn = await catched.promise;
-    return catched.conn;
-    
+  if (connection.isConnected) {
+    return;
+  }
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error('MONGODB_URI environment variable not set');
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'qickcart',
+    });
+    connection.isConnected = db.connections[0].readyState;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw error;
+  }
 }
 
 export default connectDb;
