031d677c6a0a2e568e1bd33fac67228b944b7f0b
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-04-19 00:55:56 +0300

    feat: implement POST API route for creating user orders and clearing cart

---
+++ app/api/order/create/route.js
@@ -0,0 +1,47 @@
+import { inngest } from "@/config/inngest";
+import Product from "@/models/Product";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function POST(request) {
+    try{
+        const {userId} = getAuth()
+        const {address, items} = await request.json()
+
+        if(!address ||  items.length === 0){
+            return NextResponse.json({success:false,  message: "Please provide address and items"})  
+        }
+
+        //calculate total amount
+
+        const amount= await items.reduce(async (acc, item) => {
+            const product = await Product.findById(item.product);
+            return acc + product.offerPrice * item.quantity;
+        },0)
+
+        await inngest.send({
+            name: "create-user-order",
+            data: {
+                userId: userId.userId,
+                items: items,
+                amount: amount,
+                address: address,
+                amount: amount + Math.floor(amount * 0.02 ),
+                date: Date.now(),
+            }
+        })
+
+        // clear user cart
+        const user = await User.findById(userId)
+        user.cartItems= {}
+        await user.save()
+
+        return NextResponse.json({success:true, message: "Order created successfully"})
+    }
+    catch(err){
+        console.log(err)
+        return NextResponse.json({success:true, message: err.message})
+    }   
+}
